41f5b0001d808f5bc0c83a2486b8abd2a4185a52
Matt Robinson <mthw.wm.robinson@gmail.com>
2020-08-30 14:03:01 -0400

    adding results to contributor script

--- code/contributor_pct.sql
+++ code/contributor_pct.sql
@@ -9,3 +9,19 @@ on a.package_id = b.package_id
 INNER JOIN open_source.network_centrality c
 ON (c.package = b.package) AND (c.organization = b.organization) and (a.user_id = c.user_id)
 WHERE commit = 0 and  betweenness_centrality > 0.01
+
+-- 0 Threshold
+-- Central - 85,775
+-- Central and Committed - 69,800
+
+-- Not Central - 37,975
+-- Not Central and Committed - 7,517
+
+
+-- 0.01 Threshold
+
+-- Central - 60,923
+-- Central and Committed - 58,282
+
+-- Not Central - 62,284
+-- Not Central and Committed - 19,069
